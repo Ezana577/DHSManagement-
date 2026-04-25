6abca0e334171d96e6211b99e96024b10c4f365a
Ezana577 <ezana071@gmail.com>
2026-04-25 11:15:37 -0400

    Update prefix.js

--- src/commands/prefix.js
+++ src/commands/prefix.js
@@ -1,11 +1,11 @@
-import { SlashCommandBuilder, EmbedBuilder } from ‘discord.js’;
-import { setPrefix, getPrefix } from ‘../utils/prefixStore.js’;
-import { Style } from ‘../utils/style.js’;
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { setPrefix, getPrefix } from '../utils/prefixStore.js';
+import { Style } from '../utils/style.js';
 
 const ALLOWED_ROLES = [
-‘1400533620610957493’,
-‘1496312707907977387’,
-‘1496619580188004415’,
+'1400533620610957493',
+'1496312707907977387',
+'1496619580188004415',
 ];
 
 function hasAllowedRole(member) {
@@ -13,29 +13,29 @@ return ALLOWED_ROLES.some((id) => member.roles.cache.has(id));
 }
 
 export const data = new SlashCommandBuilder()
-.setName(‘prefix’)
-.setDescription(‘Change the bot prefix.’)
+.setName('prefix')
+.setDescription('Change the bot prefix.')
 .addStringOption((option) =>
-option.setName(‘new_prefix’).setDescription(‘The new prefix to set.’).setRequired(true)
+option.setName('new_prefix').setDescription('The new prefix to set.').setRequired(true)
 );
 
 export async function execute(interaction) {
 if (!hasAllowedRole(interaction.member)) {
-await interaction.reply({ content: ‘You do not have permission to use this command.’, ephemeral: true });
+await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
 return;
 }
 
-const newPrefix = interaction.options.getString(‘new_prefix’);
+const newPrefix = interaction.options.getString('new_prefix');
 const oldPrefix = getPrefix();
 setPrefix(newPrefix);
 
 const embed = new EmbedBuilder()
 .setColor(Style.color)
-.setTitle(‘Prefix Updated’)
+.setTitle('Prefix Updated')
 .addFields(
-{ name: ‘Previous Prefix’, value: `\`${oldPrefix}``, inline: true }, { name: 'New Prefix', value: ``${newPrefix}``, inline: true }
+{ name: 'Previous Prefix', value: `\`${oldPrefix}``, inline: true }, { name: 'New Prefix', value: ``${newPrefix}``, inline: true }
 )
-.setFooter(Style.footer(’/prefix’));
+.setFooter(Style.footer('/prefix'));
 
 await interaction.reply({ embeds: [embed] });
 
