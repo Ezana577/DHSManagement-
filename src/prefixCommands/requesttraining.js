20669a4b7fb01f20d12a65023e438bfd2def1ec6
Ezana577 <ezana071@gmail.com>
2026-05-05 08:40:57 -0400

    Create requesttraining.js

---
+++ src/prefixCommands/requesttraining.js
@@ -0,0 +1,37 @@
+const ALLOWED_ROLE = '1496636002650030230';
+const REQUEST_CHANNEL_ID = '1445518845606498479';
+const PING_ROLE_ID = '1496636002650030230';
+
+export const name = 'requesttraining';
+
+export async function execute(message, args) {
+  if (message.channelId !== REQUEST_CHANNEL_ID) return;
+
+  if (!message.member.roles.cache.has(ALLOWED_ROLE)) {
+    const reply = await message.reply({ content: 'You do not have permission to use this command.' });
+    setTimeout(() => reply.delete().catch(() => null), 5000);
+    message.delete().catch(() => null);
+    return;
+  }
+
+  const target = message.mentions.users.first();
+
+  if (!target) {
+    const reply = await message.reply({ content: 'You must mention the user requesting a training.' });
+    setTimeout(() => reply.delete().catch(() => null), 5000);
+    message.delete().catch(() => null);
+    return;
+  }
+
+  message.delete().catch(() => null);
+
+  await message.channel.send({
+    content: `<@&${PING_ROLE_ID}>`,
+    allowedMentions: { roles: [PING_ROLE_ID] },
+  });
+
+  await message.channel.send({
+    content: `<@${target.id}> is requesting a training at this time. Please train them if you are available.`,
+    allowedMentions: { users: [target.id] },
+  });
+}
