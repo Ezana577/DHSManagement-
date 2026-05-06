e872db85cc4f7b0750130a30b2329f33ca20d3b8
Ezana577 <ezana071@gmail.com>
2026-05-05 21:24:44 -0400

    Update say.js

--- src/commands/say.js
+++ src/commands/say.js
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder, EmbedBuilder, MessageFlags } from 'discord.js';
 
-const ALLOWED_USERS = ['1400533620610957493', '1496619580188004415', '1496312707907977387'];
+const ALLOWED_ROLES = ['1400533620610957493', '1496619580188004415', '1496312707907977387'];
 const LOG_CHANNEL_ID = '1400610140406808768';
 
 export const data = new SlashCommandBuilder()
@@ -11,15 +11,18 @@ export const data = new SlashCommandBuilder()
   );
 
 export async function execute(interaction) {
-  if (!ALLOWED_USERS.includes(interaction.user.id)) {
+  const member = interaction.member;
+  const hasRole = ALLOWED_ROLES.some((id) => member.roles.cache.has(id));
+
+  if (!hasRole) {
     return interaction.reply({ content: 'You are not authorized to use this command.', flags: MessageFlags.Ephemeral });
   }
 
   const message = interaction.options.getString('message');
 
-  await interaction.deferReply({ flags: MessageFlags.Ephemeral });
-  await interaction.channel.send(message);
+  await interaction.reply({ content: '\u200b', flags: MessageFlags.Ephemeral });
   await interaction.deleteReply();
+  await interaction.channel.send(message);
 
   const logChannel = await interaction.client.channels.fetch(LOG_CHANNEL_ID).catch(() => null);
   if (!logChannel) return;
