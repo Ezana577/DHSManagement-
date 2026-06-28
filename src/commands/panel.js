0dd709513d76529c0d17bb047ff6dc7bdfdcab56
Ezana577 <ezana071@gmail.com>
2026-06-28 08:56:54 -0400

    Update panel.js

--- src/commands/panel.js
+++ src/commands/panel.js
@@ -49,7 +49,7 @@ function buildPanelEmbed() {
     return new EmbedBuilder()
         .setColor(EMBED_COLOR)
      .setDescription(
-`# Department Of Homeland Security support center!
+`# Department Of Homeland Security Scupport Center!
 
 > Welcome to the Department of Homeland Security Support Center. Please select the category below that best matches your request.`
 )
