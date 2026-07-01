1baffd990082214bc5b19e57efd822edb5240d60
Ezana577 <ezana071@gmail.com>
2026-07-01 00:42:46 -0400

    Update ticketCommands.js

--- src/ticketCommands.js
+++ src/ticketCommands.js
@@ -697,7 +697,7 @@ const editpanel = {
 const switchpanel = {
     data: new SlashCommandBuilder()
         .setName('switchpanel')
-        .setDescription("Switch this ticket's category. [HR+]")
+        .setDescription("Switch this ticket's category.")
         .addStringOption(o => o.setName('category').setDescription('Category to switch to.').setRequired(true)
             .addChoices(
                 { name: "❓ General Inquiry's", value: 'general' },
@@ -896,7 +896,7 @@ const rename = {
 const transfer = {
     data: new SlashCommandBuilder()
         .setName('transfer')
-        .setDescription('Transfer the claim/handler of this ticket to another staff member.')
+        .setDescription('Transfer the handler of this ticket to another support member.')
         .addUserOption(o => o.setName('user').setDescription('Staff member to transfer the claim to.').setRequired(true)),
 
     async execute(interaction) {
