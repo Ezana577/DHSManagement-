86938e2aa5671d06dbfd25db41d6e4ddf340ceaf
Ezana577 <ezana071@gmail.com>
2026-07-04 12:14:53 -0400

    Update deployment.js

--- src/commands/deployment.js
+++ src/commands/deployment.js
@@ -17,6 +17,7 @@ const BYPASS_ROLE = '1496312707907977387';
 const DEPLOYMENT_CHANNEL_ID = '1400527251748946031';
 const LOG_CHANNEL_ID = '1441817740791910551';
 const PING_ROLE_ID = '1447274909775691959';
+const PING_ROLE_ID_2 = '1519373671553040464';
 const COOLDOWN_MS = 2 * 60 * 60 * 1000;
 const BANNER_URL = 'https://media.discordapp.net/attachments/1400947813365584025/1519755229611036772/image.png';
 const DHS_EMOJI = '<:DHS:1520047343016087633>';
@@ -64,7 +65,7 @@ function buildActiveContainer(hostId, cohostId, note, startTs) {
       new TextDisplayBuilder().setContent(`## ${DHS_EMOJI} Deployment`)
     )
     .addTextDisplayComponents(
-      new TextDisplayBuilder().setContent(`<@&${PING_ROLE_ID}>`)
+      new TextDisplayBuilder().setContent(`<@&${PING_ROLE_ID}> <@&${PING_ROLE_ID_2}>`)
     )
     .addSeparatorComponents(
       new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true)
@@ -220,7 +221,7 @@ export async function execute(interaction) {
   const sent = await channel.send({
     components: [container],
     flags: MessageFlags.IsComponentsV2,
-    allowedMentions: { roles: [PING_ROLE_ID] },
+    allowedMentions: { roles: [PING_ROLE_ID, PING_ROLE_ID_2] },
   });
 
   await sent.react('✅');
